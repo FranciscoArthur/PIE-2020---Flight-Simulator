1af5ff3fda3eccba1061ffb12cfc87c8209087b5
LouisN4 <49881944+LouisN4@users.noreply.github.com>
2021-02-10 10:30:03 +0100

    Add thrust and minor changes

--- Main codes/plane_data.py
+++ Main codes/plane_data.py
@@ -1,10 +1,12 @@
 import numpy as np
 import json
+import importlib
+import atmospheric_parameters
 
 
 def plane_data(plane_position, plane_orientation,
                plane_speed, plane_angular_speed,
-               wind, pilot_data):
+               wind, pilot_data,plane):
     """
         Inputs: -plane_position: vector 3*1 [x, y, z]'
                 -plane_orientation: vector 3*1 
@@ -19,14 +21,12 @@ def plane_data(plane_position, plane_orientation,
                 -Pitching moment: Cl
                 -Rolling moment: Cm
                 -Yawing moment: Cn
+                -thrust
     """
     d2r = np.pi / 180
     r2d = 1 / d2r
-    plane = 'cessna_172'
-
-    # load data from the plane
-    with open('../Aircrafts/c172.json') as json_file:
-        data = json.load(json_file)
+    plane_file='Aircrafts.'+plane
+    planemod=importlib.import_module(plane_file)
 
     # Speed of the airplane
     global_speed = plane_speed + wind
@@ -39,49 +39,48 @@ def plane_data(plane_position, plane_orientation,
     beta = np.arcsin(global_speed[1] / v)
 
     # Define coefficients of multiplication for contribution of rotations
-    b2v = data[plane]['geometry']['bw'] / (2 * v)
-    c_bar2v = data[plane]['geometry']['cBar'] / (2 * v)
+    b2v = planemod.data['span'] / (2 * v)
+    c_bar2v = planemod.data['chord'] / (2 * v)
 
     # Define control parameters angle
     q = plane_angular_speed[0]
     p = plane_angular_speed[1]
     r = plane_angular_speed[2]
-    de = pilot_data[1] / 20 * data[plane]['amplitudes']['de'] * d2r
-    da = pilot_data[2] / 20 * data[plane]['amplitudes']['da'] * d2r
-    dr = pilot_data[3] / 20 * data[plane]['amplitudes']['dr'] * d2r
+    de = pilot_data[1] / 20 * planemod.data['de_max'] * d2r
+    da = pilot_data[2] / 20 * planemod.data['da_max'] * d2r
+    dr = pilot_data[3] / 20 * planemod.data['dr_max'] * d2r
+    dthrust=pilot_data[0]/10
 
-    # Force characteristics
-    coeffs = data[plane]['force_coefficients']
 
     # Lift coefficient
-    coeffs_lift = coeffs['lift']
-    cl = coeffs_lift['CL_0'] + coeffs_lift['CL_a'] * alpha + coeffs_lift['CL_q'] * q * c_bar2v + coeffs_lift[
+    cl = planemod.data['CL_0'] + planemod.data['CL_a'] * alpha + planemod.data['CL_q'] * q * c_bar2v + planemod.data[
         'CL_de'] * de
 
     # Drag coefficient
-    coeffs_drag = coeffs['drag']
-    cd = coeffs_drag['CD_0'] + coeffs_drag['induced_drag_factor'] * cl ** 2 + coeffs_drag['CD_de'] * de
+    cd = planemod.data['CD_0'] + planemod.data['induced_drag_factor'] * cl ** 2 + planemod.data['CD_de'] * de
 
     # Side force coefficient
-    coeffs_sf = coeffs['side_force']
-    cy = coeffs_sf['CY_0'] + coeffs_sf['CY_beta'] * beta + (coeffs_sf['CY_p'] * p + coeffs_sf['CY_r'] * r) * b2v + \
-         coeffs_sf['CY_dr'] * dr
+    cy = planemod.data['CY_0'] + planemod.data['CY_beta'] * beta + (planemod.data['CY_p'] * p + planemod.data['CY_r'] * r) * b2v + \
+         planemod.data['CY_dr'] * dr
 
     # Moment characteristics
 
     # Pitching moment
-    coeffs_pitch = coeffs['pitch']
-    cm = coeffs_pitch['Cm_0'] + coeffs_pitch['Cm_da'] * da + coeffs_pitch['Cm_q'] * c_bar2v * q + coeffs_pitch[
+    cm = planemod.data['Cm_0'] + planemod.data['Cm_da'] * da + planemod.data['Cm_q'] * c_bar2v * q + planemod.data[
         'Cm_de'] * de
 
     # Rolling moment
-    coeffs_roll = coeffs['roll']
-    cl = coeffs_roll['Cl_0'] + coeffs_roll['Cl_da'] * da + coeffs_roll['Cl_beta'] * beta + (
-            coeffs_roll['Cl_r'] * r + coeffs_roll['Cl_p'] * p) * b2v * coeffs_roll['Cl_dr'] * dr
+    cl = planemod.data['Cl_0'] + planemod.data['Cl_da'] * da + planemod.data['Cl_beta'] * beta + (
+            planemod.data['Cl_r'] * r + planemod.data['Cl_p'] * p) * b2v * planemod.data['Cl_dr'] * dr
 
     # Yawing moment
-    coeffs_yaw = coeffs['yaw']
-    cn = coeffs_yaw['Cn_0'] + coeffs_yaw['Cn_beta'] * beta + (coeffs_yaw['Cn_p'] * p + coeffs_yaw['Cn_r'] * r) * b2v + \
-         coeffs_yaw['Cn_da'] * da + coeffs_yaw['Cn_dr'] * dr
+    cn = planemod.data['Cn_0'] + planemod.data['Cn_beta'] * beta + (planemod.data['Cn_p'] * p + planemod.data['Cn_r'] * r) * b2v + \
+         planemod.data['Cn_da'] * da + planemod.data['Cn_dr'] * dr
 
-    return [cl, cd, cy, cl, cm, cn]
+
+    #Thrust
+    atm_params=atmospheric_parameters.atmospheric_parameters_fct(plane_position[2])
+    air_density=atm_params[4]
+    thrust=dthrust*planemod.data['static_thrust']*(air_density/1.225)#*(1-np.exp((plane_position[2]-18000)/2000))    
+
+    return [cl, cd, cy, cl, cm, cn, thrust]
