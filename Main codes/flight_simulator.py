49bd993a99c4c712ed0e1db0edbc4f328b9b139b
Clem-99 <75092244+Clem-99@users.noreply.github.com>
2021-02-15 22:31:21 +0100

    Revert "Update flight_simulator"
    This reverts commit 5bfdb2c81bf1e3d011c7e440c6c599e7cb8f6262.

--- Main codes/flight_simulator.py
+++ Main codes/flight_simulator.py
@@ -307,7 +307,7 @@ def Flight_Simulator_fct(plane, initial_conditions, weather, integration_paramet
         
         plane_current_fuel_consumption = fuel_consumption_calculation_fct(current_pilot_controls[0])
         
-        # plane_current_fuel_consumption=1
+        plane_current_fuel_consumption=1
         
         
         
@@ -329,16 +329,16 @@ def Flight_Simulator_fct(plane, initial_conditions, weather, integration_paramet
         
         from solver import rk4
         current_state_vector = rk4(plane_position_before_update, 
-                                   plane_orientation_before_update, 
-                                   plane_speed_before_update, 
-                                   plane_angular_speed_before_update, 
-                                   plane_intrinsic_data, 
-                                   delta_t, 
-                                   atmospheric_parameters_before_update, 
-                                   plane_current_forces, 
-                                   plane_current_moments,
-                                   wind,
-                                   plane_current_mass)
+                                          plane_orientation_before_update, 
+                                          plane_speed_before_update, 
+                                          plane_angular_speed_before_update, 
+                                          plane_intrinsic_data, 
+                                          delta_t, 
+                                          atmospheric_parameters_before_update, 
+                                          plane_current_forces, 
+                                          plane_current_moments,
+                                          wind,
+                                          plane_current_mass)
 
 
         # CHECK NOMENCLATURE WITH ARTHUR - CHECK ORDER OF OUTPUTS
@@ -378,8 +378,8 @@ def Flight_Simulator_fct(plane, initial_conditions, weather, integration_paramet
     result = {}
     result['plane_position'] = plane_position
     result['plane_orientation'] = plane_orientation
-    result['plane_speed'] = plane_speed                    # Ground speed - vector
-    result['plane_TAS'] = plane_TAS                        # True AirSpeed - scalar
+    result['plane_speed'] = plane_speed                    # Ground speed
+    result['plane_TAS'] = plane_TAS                        # True AirSpeed
     result['plane_angular_speed'] =   plane_angular_speed
     result['plane_fuel_load'] = plane_fuel_load
     result['plane_mass'] = plane_mass
