19ba3f16f6aee63ba215eccce0d73368d31ccd86
LouisN4 <49881944+LouisN4@users.noreply.github.com>
2021-03-06 09:50:17 +0100

    Modifications and add mach effect on coefficients

--- Main codes/plane_data.py
+++ Main codes/plane_data.py
@@ -4,7 +4,7 @@ import numpy as np
 def plane_data_fct(plane_position, plane_orientation,
                plane_speed, plane_angular_speed,atmospheric_parameters,plane_intrinsic_data,
                wind, pilot_data, plane_TAS_before_update, plane_TAS_vector_before_update,
-               alpha, beta):
+               alpha, beta,plane_mach):
     """
         Inputs: -plane_position: vector 3*1 [x, y, z]'
                 -plane_orientation: vector 3*1 
@@ -28,8 +28,6 @@ def plane_data_fct(plane_position, plane_orientation,
     # Speed of the airplane in the air
     v_air_mod = plane_TAS_before_update
 
-    # alpha = np.arctan(v_air_vec[2] / abs(v_air_vec[0]))  # FAUX CAR REPERE TERRESTRE
-    # beta = np.arcsin(v_air_vec[1] / v_air_mod)           # FAUX CAR REPERE TERRESTRE
 
     # Define coefficients of multiplication for contribution of rotations
     b2v = plane_intrinsic_data['span'] / (2 * v_air_mod)
@@ -44,36 +42,36 @@ def plane_data_fct(plane_position, plane_orientation,
     da = pilot_data[2] / 10 * plane_intrinsic_data['da_max'] * d2r
     dr = pilot_data[1] / 10 * plane_intrinsic_data['dr_max'] * d2r
     dthrust=pilot_data[0]/10
-
-
+    #Prandtl factor for mach effect
+    prandtl=1/np.sqrt(1-plane_mach**2)
+    
     # Lift coefficient
-    cL = plane_intrinsic_data['CL_0'] + plane_intrinsic_data['CL_a'] * alpha + plane_intrinsic_data['CL_q'] * q * c_bar2v + plane_intrinsic_data['CL_de'] * de
+    cL = (plane_intrinsic_data['CL_0'] + plane_intrinsic_data['CL_a'] * alpha + plane_intrinsic_data['CL_q'] * q * c_bar2v + plane_intrinsic_data['CL_de'] * de)*prandtl
 
     # Drag coefficient
-    cd = plane_intrinsic_data['CD_0'] + plane_intrinsic_data['induced_drag_factor'] * cL ** 2 + plane_intrinsic_data['CD_de'] * de
+    cd = (plane_intrinsic_data['CD_0'] + plane_intrinsic_data['induced_drag_factor'] * cL ** 2 + plane_intrinsic_data['CD_de'] * de)*prandtl
 
     # Side force coefficient
-    cy = plane_intrinsic_data['CY_0'] + plane_intrinsic_data['CY_beta'] * beta + (plane_intrinsic_data['CY_p'] * p + plane_intrinsic_data['CY_r'] * r) * b2v + plane_intrinsic_data['CY_dr'] * dr
+    cy = (plane_intrinsic_data['CY_0'] + plane_intrinsic_data['CY_beta'] * beta + (plane_intrinsic_data['CY_p'] * p + plane_intrinsic_data['CY_r'] * r) * b2v + plane_intrinsic_data['CY_dr'] * dr)*prandtl
 
 
 
 
     # Moment characteristics
     # Pitching moment
-    cm = plane_intrinsic_data['Cm_0'] + plane_intrinsic_data['Cm_da'] * da + plane_intrinsic_data['Cm_q'] * c_bar2v * q + plane_intrinsic_data['Cm_de'] * de
+    cm = (plane_intrinsic_data['Cm_0'] + plane_intrinsic_data['Cm_a'] * alpha + plane_intrinsic_data['Cm_q'] * c_bar2v * q + plane_intrinsic_data['Cm_de'] * de)*prandtl
 
 
     # Rolling moment
-    cl = plane_intrinsic_data['Cl_0'] + plane_intrinsic_data['Cl_da'] * da + plane_intrinsic_data['Cl_beta'] * beta + (
-            plane_intrinsic_data['Cl_r'] * r + plane_intrinsic_data['Cl_p'] * p) * b2v * plane_intrinsic_data['Cl_dr'] * dr
+    cl = (plane_intrinsic_data['Cl_0'] + plane_intrinsic_data['Cl_da'] * da + plane_intrinsic_data['Cl_beta'] * beta + (
+            plane_intrinsic_data['Cl_r'] * r + plane_intrinsic_data['Cl_p'] * p) * b2v * plane_intrinsic_data['Cl_dr'] * dr)*prandtl
 
     # Yawing moment
-    cn = plane_intrinsic_data['Cn_0'] + plane_intrinsic_data['Cn_beta'] * beta + (plane_intrinsic_data['Cn_p'] * p + plane_intrinsic_data['Cn_r'] * r) * b2v + \
-          plane_intrinsic_data['Cn_da'] * da + plane_intrinsic_data['Cn_dr'] * dr
+    cn = (plane_intrinsic_data['Cn_0'] + plane_intrinsic_data['Cn_beta'] * beta + (plane_intrinsic_data['Cn_p'] * p + plane_intrinsic_data['Cn_r'] * r) * b2v + 
+          plane_intrinsic_data['Cn_da'] * da + plane_intrinsic_data['Cn_dr'] * dr)*prandtl
 
 
     #Thrust
     air_density=atmospheric_parameters[4]
-    thrust=dthrust*plane_intrinsic_data['static_thrust']*(air_density/1.225)#*(1-np.exp((plane_position[2]-18000)/2000))    
-    
+    thrust=dthrust*plane_intrinsic_data['static_thrust']*(air_density/1.225)*(1-np.exp((plane_position[2]-18000)/2000))    
     return [cL, cd, cy, cl, cm, cn, thrust]
